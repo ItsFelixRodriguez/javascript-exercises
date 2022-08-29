85e6ec6c54a0d2fac2067c2f9869df9bf69a56b0
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-08-29 12:17:47 +0900

    can't believe I figured this one out

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
+const repeatString = function(str,times) {
+    let i;
+    var text = "";
+    for (i=0; i<times;i++){
+        text += str;
+    } 
+   
+    if (times < 0) {
+        return "ERROR";
+    }
+    return text;
 };
-
+let text = "hey";
+let times = 3;
+repeatString ()
 // Do not edit below this line
 module.exports = repeatString;
